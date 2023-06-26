54f4038bb7529620e127a12567569010d3bd161d
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-06-26 13:57:42 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -469,7 +469,7 @@
                   key: 21,
                   title: "TBB-EG09",
 		  "x": 2800,
-		  "y": 100,
+		  "y": 50,
                   attributes: [
                     {
                       label: "Technical Availability",
@@ -481,7 +481,7 @@
                   key: 22,
                   title: "TBB-EG10",
 		  "x": 2800,
-		  "y": 175,
+		  "y": 125,
                   attributes: [
                     {
                       label: "Technical Availability",
