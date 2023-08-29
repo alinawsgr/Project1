a42c418fd49fcec389c54adfaa26906e38c57b53
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-08-29 07:39:16 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -72,7 +72,8 @@
                         }
                     }
                 }
-            }     
+            }
+            matrix = source;     
         }
 
         connectedCallback() {
