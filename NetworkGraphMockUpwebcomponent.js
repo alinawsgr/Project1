7268e1f4d38afa70eef4a9e361b43d562d843a1b
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-09-04 14:20:32 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -49,7 +49,7 @@
             var Machines = []; // contains names of existing machines
             var t_source = []; // contains the transformed xml-structure
 
-            //source = Array(74);
+            source = Array(74);
             // get all machines -> Machines contains all existing machines
             for(i = 0; i < source.length; i++){
                 Machines.push({Machine: source[i].Quelle_.id.replace(" ", "-")})
