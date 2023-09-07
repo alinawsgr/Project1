1c4d2827562fdcf97bae1e67ec3aa7f6a1e38982
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-09-07 13:29:44 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -409,25 +409,24 @@
 
         function findPaths(t_source, priority){
             // calculates the row of the start machine, from where the paths start
-            let startMachine = ['Entlader']; // nicht optimal -> sollte man besser in Excel fix vorgeben wo der genaue Startpunkt ist
+            let startMachine = 'Entlader'; // nicht optimal -> sollte man besser in Excel fix vorgeben wo der genaue Startpunkt ist
             // path1 contains all connections with value 1 -> Hauptlinie
             let path = [];
             // define "Entlader" as the start Position for the path
             for (let i=0; i<t_source.length; i++){
                 if (t_source[i].ID === priority){
-                    if (t_source[i].Parent_Machine === 'Entlader'){
+                    if (t_source[i].Parent_Machine === startMachine){
                         startMachine = t_source[i];
                         path.push(startMachine.Parent_Machine);
                         path.push(startMachine.Children_Machine);                       
                     }
-                        for (let j=1; j<path.length; j++){
-                            currM = [];
-                            currM = path[j];
-                            if(t_source[i].Parent_Machine === currM.Parent_Machine){
-                                path.push(t_source[i].Children_Machine);
-                        }
-
-
+                }
+            }
+            
+            for (let j=1; j<path.length; j++){
+                for (let a=0; a<t_source.length; a++){
+                    if(t_source[a].Parent_Machine === path[j].Parent_Machine){
+                        path.push(t_source[a].Children_Machine);
                     }
                 }
             }
