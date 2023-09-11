01873c11839a021946e74659de6caa65ef762f6e
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-09-11 14:38:20 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -502,7 +502,7 @@
         // 1
         calcpositionshor_r(path1_hor_r, t_source, nodeWidth, '1');
         calcpositionssenkr_u(path1_senkr_u, t_source, nodeHeight,'1');
-        //calcpositionshor_l (path1_hor_l,t_source,nodeWidth,'1');
+        calcpositionshor_l (path1_hor_l,t_source,nodeWidth,'1');
         // 2
         //calcpositionssenkr_u(path2,t_source,nodeHeight);
         // others...
