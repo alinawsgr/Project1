b245ff8c8b2cb0bb716b8a6ca36600b02116d61d
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-09-18 15:25:02 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -358,7 +358,7 @@
         let path1_senkr_u = path1.slice((path1.indexOf(directionChange[0])), (path1.indexOf(directionChange[1]) + 1));
         let path1_hor_l = path1.slice((path1.indexOf(directionChange[1])),( path1.length));
         let indexVar = path1_hor_l.indexOf('Varioline');
-        let path1_hor_l_1 = path1_hor_l.slice(0, indexVar-1);
+        let path1_hor_l_1 = path1_hor_l.slice(0, indexVar);
         let path1_hor_l_2 = path1_hor_l.slice (indexVar, path1_hor_l.length);
         console.log(path1_hor_l_1);
         console.log(path1_hor_l_2);
