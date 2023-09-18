65b478c66fc2dae8fe61ecabe2972c3a24aba1f2
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-09-18 14:53:15 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -456,7 +456,7 @@
                 }
         
                 for (let a = 0; a < t_source.length; a++) {
-                    if (t_source[a].Parent_Machine === path[i + 1]) {
+                    if (t_source[a].Parent_Machine === path[i + 1] && path[i+1] != 'Varioline') {
                         t_source[a].X = firstMachineX + xOffset;
                         t_source[a].Y = firstMachineY + yOffset;
                     }
