bf4c2cc4e37f0c41d5b016709dc3d39d10968b47
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-09-28 13:20:54 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -620,7 +620,7 @@
             let elementsbetween = path.length - 3; 
             let pathlength_X = (endX- startX) / elementsbetween 
             //let pathlength_Y = endY - endY; 
-            for (let j=1; j<(path.length)-1; j++){
+            for (let j=2; j<(path.length)-1; j++){
                 let offset = 0;
                 let nextMachine = path[j];
                 for (let e=0; e<t_source.length; e++){
