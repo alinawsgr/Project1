777854b80768e8df5de1615e1db214231d417c0f
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-10-04 08:33:02 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -758,8 +758,8 @@
         let endmachines = [];
         for (let j=0; j<allchildrenmachines.length; j++){
             for (let x=0; x<allmothermachines.length; x++){
-                if ((isInArray(allchildrenmachines[j], allchildrenmachines)) === false){
-                    if ((isInArray(allchildrenmachines[j], allmothermachines)) === false){
+                if (isInArray(allchildrenmachines[j], allchildrenmachines) === false){
+                    if (isInArray(allchildrenmachines[j], allmothermachines) === false){
                         endmachines.push(allchildrenmachines[j]);
                     }
                 }  
