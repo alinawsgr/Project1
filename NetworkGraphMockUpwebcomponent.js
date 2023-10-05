0f82835e248df3c2a084a82cbbd8d4e583b4e06e
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-10-05 16:28:13 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -829,34 +829,17 @@
             }
         }
         console.log(singlechilds);
+
+
+        let singleparentchild = {};
+        for (let o=0; o<singleparent; o++){
+            singleparentchild[singleparent[i]] = singlechilds[i];
+        }
+        console.log(singleparentchild);
         
         // place singlechild inside the graph
         let parentsy = 0;
         let parentsx = 0;
-        for (let sp = 0; sp<singleparent.length; sp++){
-            for (let i=0; i<t_source.length; i++){
-                if(t_source[i].Parent_Machine === singleparent[sp]){
-                    parentsy = t_source[i].Y;
-                    parentsx = t_source[i].X;
-                }
-            }
-            for (let sc = 0; sc<singlechilds.length; sc++){
-                let space = nodeWidth;
-                let xOffset = space;
-                let yOffset = 0;
-                let yvalue = parentsy + yOffset;
-                let xvalue = parentsx - xOffset;
-                t_source.push( { // push end machines into t_source with undefined parent machine so that the psoition can be stored
-                    Parent_Machine: singlechilds[sc],
-                    X: xvalue,
-                    Y: yvalue,
-                    ID: 'singleend',
-                    Children_Machine: '',
-                    X_dep: '',
-                    Y_dep: ''});
-            } 
-
-        }
 
         
        
