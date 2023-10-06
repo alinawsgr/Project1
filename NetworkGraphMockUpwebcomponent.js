c4213a4a4ca8b1f68619ed5f6a0d0aaa07bac8eb
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-10-06 09:47:40 +0200

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -834,21 +834,34 @@
 
         let singleparentchild = {};
         singleparent.forEach((key,index) => {singleparentchild[key] = singlechilds[index];})
-        console.log(singleparentchild);
+        console.log(singleparentchild); // {TBB_EG22: 'TBB_EG25', Fueller: 'TBB_EG20', TBP1_EG08: 'Extern_Abgabe_PAL'}
         
         // place singlechild inside the graph
         let parentsy = 0;
         let parentsx = 0;
-
-        
-       
-
-    
-
-
-
-
-
+        for (let sp = 0; sp<singleparent.length; sp++){
+            for (let i=0; i<t_source.length; i++){
+                if(t_source[i].Parent_Machine === singleparent[sp]){
+                    parentsy = t_source[i].Y;
+                    parentsx = t_source[i].X;
+                }
+            }
+        }
+        for (let sc = 0; sc<singlechilds.length; sc++){
+            let space = nodeHeight/ multichilds.length;
+            let xOffset = space;
+            let yOffset = 0;
+            let yvalue = parentsy + yOffset;
+            let xvalue = parentsx - xOffset ;
+            t_source.push( { // push end machines into t_source with undefined parent machine so that the psoition can be stored
+                Parent_Machine: singlechilds[sc],
+                X: xvalue,
+                Y: yvalue,
+                ID: 'singleend',
+                Children_Machine: '',
+                X_dep: '',
+                Y_dep: ''});
+        } 
         console.log(t_source);
 
 
