b74e6c919c76427cb1210324da20533d235aa7c7
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-12-04 10:26:56 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -680,17 +680,22 @@
 
         for (let p=0; p<paths_10.length; p++){
             let path10 = []
+            var found = false;
             for (let i=0; i<t_source.length; i++){
                 if (paths_10[p] === t_source[i].Parent_Machine){
                     if (t_source[i].ID === '10'){
-                        path10.push(t_source[i].Parent_Machine);
-                        console.log(path10);
+                        found = true; 
+                        break;  
                     }
-                }
+                }  
             }
-            
+            if (found){
+                path10.push(t_source[i].Parent_Machine);
+                console.log(path10);
+            }
         }
-        
+            
+
             
         //let currentpath2 = paths_10[p];
             //calcpositionsexternal(currentpath2); /// hier noch problem
