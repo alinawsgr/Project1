8a5b91d5dad4d13de7fe653fccae1b0c6bac2127
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-12-04 10:34:40 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -680,18 +680,13 @@
 
         for (let p=0; p<paths_10.length; p++){
             let path10 = []
-            var found = false;
             for (let i=0; i<t_source.length; i++){
                 if (paths_10[p] === t_source[i].Parent_Machine){
-                    if (t_source[i].ID === '10'){
-                        found = true; 
-                        break;  
+                    if (t_source[i].ID === '10'){ 
+                        path10.push(t_source[i].Parent_Machine);
+                        console.log(path10);
                     }
-                }  
-            }
-            if (found){
-                path10.push(t_source[i].Parent_Machine);
-                console.log(path10);
+                }
             }
         }
             
