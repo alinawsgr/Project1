e94494e59690d01de91c1161125e16350d17eb4c
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2023-12-22 10:28:50 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -730,7 +730,7 @@
             }
             for (let g=0; g<t_source.length; g++){
                 if (t_source[g].Parent_Machine === path10_t[p]){
-                        // if there is alredy a machine, place it the other way round
+                        // if there is already a machine, place it the other way round
                         for (let c=0; c<t_source.length; c++){
                             if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
                                 t_source[g].X = parentx_;
@@ -1024,22 +1024,6 @@
     }
     placeleftAbschieberpath();
 
-
-    // place or delete not placed machines
-
-     /*TBG_EG08 and TBB_EG11
-    for (let p=0; p<t_source.length; p++){
-        if (t_source[p].Parent_Machine == 'TBG_EG08'){
-            t_source[p].X = 8000
-            t_source[p].Y = 6000
-            t_source[p].Children_Machine = ''
-        }
-        if (t_source[p].Parent_Machine == 'TBB_EG11'){
-            t_source[p].X = 8500
-            t_source[p].Y = 6000
-            t_source[p].Children_Machine = ''
-        } 
-    }*/
 
 //  delete ends with value 0 in coordinates
     for (let e=0; e<t_source.length; e++){
