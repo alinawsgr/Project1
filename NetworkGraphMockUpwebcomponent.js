cfbad0121ed556a12f76219c4b5ce0c908f1d148
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2024-02-26 10:08:53 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -744,10 +744,8 @@
                         if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
                             t_source[g].X = parentx_;
                             t_source[g].Y = childreny;
-                    
-                          
                         }
-                        if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)){
+                        else if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)){
                             t_source[g].X = childrenx;
                             t_source[g].Y = parenty_;
                            
