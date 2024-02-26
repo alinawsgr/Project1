e5e5d5d2c293645e2697c18b05f6e9d4bb298083
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2024-02-26 15:03:02 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -764,7 +764,7 @@
                         if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
                             t_source[g].X = parentx_;
                             t_source[g].Y = childreny;
-                            continue;
+                            //continue;
                         }
                         /*else if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)) {
                             t_source[g].X = childrenx;
