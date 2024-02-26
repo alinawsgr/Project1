7a67658cbe66a9fbdb1ca3c8eb325b93ad7a9a4a
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2024-02-26 13:55:51 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -743,9 +743,27 @@
                 if (t_source[g].Parent_Machine === path10_t[p]){
                         // if there is already a machine, place it the other way round
                     for (let c=0; c<t_source.length; c++){
+                        if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
+                            t_source[g].X = parentx_;
+                            t_source[g].Y = childreny;
+                            break;
+                        }
+                        else if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)){
+                            t_source[g].X = childrenx;
+                            t_source[g].Y = parenty_;
+                            break;
+                        }
+                    }
+                        
+                }
+            }
+            /*for (let g=0; g<t_source.length; g++){
+                if (t_source[g].Parent_Machine === path10_t[p]){
+                        // if there is already a machine, place it the other way round
+                    for (let c=0; c<t_source.length; c++){
                         if ((t_source[c].X === t_source[g].X) || (t_source[c].Y === t_source[g].Y)){
-                            t_source[g].X = parentx_ + 100;
-                            t_source[g].Y = childreny + 100;
+                            t_source[g].X = parentx_;
+                            t_source[g].Y = childreny;
                         }
                         else if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)) {
                             t_source[g].X = childrenx;
@@ -753,8 +771,8 @@
                         }
                     }
                         
-                }
-            }
+                }*/
+            
         }
                
 
