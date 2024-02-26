fd5a7561dc6964f20b8e3311c740cd616d36ba13
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2024-02-26 16:06:27 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -742,15 +742,19 @@
             for (let g=0; g<t_source.length; g++){
                 if (t_source[g].Parent_Machine === path10_t[p]){
                         // if there is already a machine, place it the other way round
+                    let loop = false;
                     for (let c=0; c<t_source.length; c++){
                         if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
                             t_source[g].X = parentx_;
                             t_source[g].Y = childreny;
+                            loop = true;
                         }
-                        if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)){
+                        if (((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)) && loop==false){
                             t_source[g].X = childrenx;
                             t_source[g].Y = parenty_;
+                            loop = true; 
                         }
+                        loop =false;
                     }      
                 }
             }
