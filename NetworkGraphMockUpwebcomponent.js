1d8308867852efcea848cc982573aed640765ed7
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2024-02-26 14:56:10 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -777,13 +777,15 @@
             }
             for (let j=0; j<t_source.length; j++){
                 if (t_source[j].Parent_Machine === path10_t[p]){
-                    if ((t_source[c].X !== t_source[j].X) || (t_source[c].Y !== t_source[j].Y)) {
-                        t_source[j].X = childrenx;
-                        t_source[j].Y = parenty_;
-                    }
+                    for (let c=0; c<t_source.length; c++){
+                        if ((t_source[c].X !== t_source[j].X) || (t_source[c].Y !== t_source[j].Y)) {
+                            t_source[j].X = childrenx;
+                            t_source[j].Y = parenty_;
+                        }
 
 
             }
+        }
             
         }
     }
