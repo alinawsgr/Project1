00e5ef8dbfdd6bd375bc8f81ca649cf98922cd1b
alinawsgr <132360561+alinawsgr@users.noreply.github.com>
2024-02-26 10:34:24 +0100

    Update NetworkGraphMockUpwebcomponent.js

--- NetworkGraphMockUpwebcomponent.js
+++ NetworkGraphMockUpwebcomponent.js
@@ -741,15 +741,14 @@
                 if (t_source[g].Parent_Machine === path10_t[p]){
                         // if there is already a machine, place it the other way round
                     for (let c=0; c<t_source.length; c++){
-                      
-                        if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)){
-                            t_source[g].X = childrenx;
-                            t_source[g].Y = parenty_;
-                        }
-                        else if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
+                        if ((t_source[c].X === t_source[g].X) && (t_source[c].Y === t_source[g].Y)){
                             t_source[g].X = parentx_;
                             t_source[g].Y = childreny;
-                            }
+                        }
+                        else if ((t_source[c].X !== t_source[g].X) || (t_source[c].Y !== t_source[g].Y)){
+                            t_source[g].X = childrenx;
+                            t_source[g].Y = parenty_;
+                            continue;
                            
                             
                         }
@@ -757,7 +756,7 @@
                         
                 }
             }
-        
+        }
                
 
         // end nodes (machines with no children machine need special handling, because they are not in T_source included and they need to be pushed in t_source as mother_machines in order to give them x and y coordinates )
